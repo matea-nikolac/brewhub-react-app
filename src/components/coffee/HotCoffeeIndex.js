f5661ed1a46cc2e41fff32df28ef38dd22c537d8
Matea <matea.nikolac95@gmail.com>
2023-11-20 22:12:09 +0100

    created the HotCoffeeIndex.js component

--- src/components/coffee/HotCoffeeIndex.js
+++ src/components/coffee/HotCoffeeIndex.js
@@ -1,8 +1,62 @@
-const HotCoffeeIndex = () => {
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Spinner from '../common/Spinner';
+import Error from '../common/Error';
 
-  return(
-    <h1>HotCoffeeIndex</h1>
-  )
+const HotCoffeeSingle = () => {
+  const [coffeeData, setCoffeeData] = useState([]);
 
-}
-export default HotCoffeeIndex
+  useEffect(() => {
+    const getCoffee = async () => {
+      try {
+        const response = await axios.get('https://api.sampleapis.com/coffee/hot');
+        setCoffeeData(response.data);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCoffee();
+  }, []);
+
+  return (
+    <main>
+      <Container>
+        <Row>
+          <Col className="h1-coffee-column" xs="12">
+            <h1 className="h1-coffee display-4 mb-8 text-center">HOT COFFEE</h1>
+          </Col>
+          <Col xs="12" className="intro-text">
+            <p className="text-center">Explore our selection of hot coffees and find your perfect brew. Each cup is crafted with care to bring you a delightful coffee experience.</p>
+          </Col>
+          {coffeeData.length > 0 &&
+            coffeeData.map((coffee) => {
+              const { id, image, title } = coffee;
+              if (id !== 20) {
+              return (
+                <Col key={id} lg="4" md="6" sm="12" className='hot-coffee-column'>
+                  <Link to={`/iced-coffee/${id}`}>
+                    <Card>
+                      <div className="card-image" style={{ backgroundImage: `url('${image}')` }}></div>
+                      <Card.Body>
+                        <Card.Text>{title.toUpperCase()}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Link>
+                </Col>
+              );
+              }
+            })}
+          
+        </Row>
+      </Container>
+    </main>
+  );
+};
+
+export default HotCoffeeSingle;
